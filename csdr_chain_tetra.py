42a32203de624a968b88d318a45154c72163b4e8
trollminer <32915724+trollminer@users.noreply.github.com>
2026-04-09 23:53:33 +0100

    Update csdr_chain_tetra.py

--- csdr_chain_tetra.py
+++ csdr_chain_tetra.py
@@ -23,7 +23,7 @@ logger = logging.getLogger(__name__)
 class Tetra(BaseDemodulatorChain, FixedIfSampleRateChain, FixedAudioRateChain, MetaProvider):
     """TETRA voice demodulator chain for OpenWebRX+."""
 
-    def __init__(self, tetra_dir: str = '/opt/openwebrx-tetra'):
+    def __init__(self, tetra_dir: str = '/usr/lib/python3/dist-packages/htdocs/plugins/receiver/tetra'):
         from csdr.module.tetra import TetraDecoderModule
         self.decoder = TetraDecoderModule(tetra_dir)
         workers = [self.decoder]
